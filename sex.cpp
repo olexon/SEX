c1cd9707443f1586d879c697f8c95269f80185fd
olexon <61621153+olexon@users.noreply.github.com>
2021-06-17 23:26:40 +0200

    Add files via upload

---
+++ sex.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <windows.h>
+
+// credits: ja bo ja to pisalem 
+
+//remixer to debil
+
+int main() {
+	SetConsoleTitleA("Cipa");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+	std::cout << "wybiesz seks stronke" << std::endl;
+	std::cout << "1 xnxx 2 pornhub 3 sexrura" << std::endl;
+	int sex;
+	std::cout << "\n";
+	std::cin >> sex;
+
+	// tutaj dzikie sexy bedo
+
+	if (sex == 1) {
+		system("start https://xnxx.com/");
+		return 0;
+	}
+	else if (sex == 2) {
+		system("start https://pornhub.com/");
+		return 0;
+	}
+
+	// wiecej seksu
+
+	else if (sex == 3) {
+		system("start https://sexrura.pl/");
+		return 0;
+	}
+	else {
+		system("cls");
+		// chuj juz po ptakach
+		std::cout << "chuj" << std::endl;
+		std::cin.get();
+	};
+	// sex.exe never crack !!!!!
+std::cin.get();
+};
